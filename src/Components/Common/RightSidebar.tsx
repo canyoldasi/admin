236dbd5898064ff2092d808089f50f6dabf81397
Saliha Atak <esrefatak@gmail.com>
2025-04-03 12:56:07 +0300

    Theme Customizer default closed

--- src/Components/Common/RightSidebar.tsx
+++ src/Components/Common/RightSidebar.tsx
@@ -98,7 +98,7 @@ const RightSidebar = () => {
         sidebarVisibilitytype
     } = useSelector(selectLayoutProperties);
     // open offcanvas
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
     const toggleLeftCanvas = () => {
         setOpen(!open);
     };
